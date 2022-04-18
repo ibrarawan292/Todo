d7e0bbaa03b637aae6bca61651f5ff6d00586c50
ibrar hussain <ibrarawan292@gmail.com>
2022-04-18 12:31:12 +0500

    display data from database to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,26 +8,30 @@ function App() {
 
   const [todos, setTodos] = useState('')
 
- 
+ async function getAllTodos(){
+   try {
+    const todos = await axios.get('http://localhost:5000/api/v1/todos')
+     .then((res) =>{
+       setTodos(res.data)
+     })
+   } catch (error) {
+     console.log(error)
+   }
+ }
 
-  useEffect(()=>{
- 
-    try {
-     const todos =  axios.get('http://localhost:5000/api/v1/todos')
-      .then((res) =>{
-        setTodos(res.todos)
-        console.log(todos)
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  
+ useEffect(()=>{
+      getAllTodos()
  },[])
 
   return (
     <div className='App'>
-      
-     
+      {
+        todos.map((todo) =>{
+          return(
+            <div>{todo.title}</div>
+          )
+        })
+      }
     </div>
   
   );
