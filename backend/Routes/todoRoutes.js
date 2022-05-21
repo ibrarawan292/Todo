76e675b8991f955b0efe51c0359f83983937fa7b
unknown <ibrarawan292@gmail.com>
2022-05-21 10:54:58 -0700

    todo route level protection

--- backend/Routes/todoRoutes.js
+++ backend/Routes/todoRoutes.js
@@ -7,13 +7,15 @@ const {
     updateTodo,
     deleteTodo
   } = require('../Controllers/todoControllers')
+  const {isAuthenticatedUser} = require('../middleware/authMiddleware')
+
   
 
 router.route('/todo/new').post(createNewTodo)
 router.route('/todos').get(getAllTodos)  //alternate way =  router.get('/todos', getAllTodos )
 router.route('/todo/:id').get(getTodoById)
-router.route('/todo/update/:id').put(updateTodo)  //api/v1/todo/update/11
-router.route('/todo/delete/:id').delete(deleteTodo)  //api/v1/todo/delete/11
+router.route('/todo/update/:id').put(isAuthenticatedUser, updateTodo)  //api/v1/todo/update/11
+router.route('/todo/delete/:id').delete(isAuthenticatedUser, deleteTodo)  //api/v1/todo/delete/11
 
 
 
--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -6,9 +6,9 @@ const {isAuthenticatedUser} = require('../middleware/authMiddleware')
 router.route('/user/login').post(login)  //api/v1/user/login
 router.route('/user/create').post(createUser) //api/v1/user/create
 router.route('/users').get( getAllUsers)  //alternate way =  router.get('/users', getAllUsers )
-router.route('/user/:id').get(getUserById)
+router.route('/user/:id').get(isAuthenticatedUser, getUserById)
 router.route('/account/logout').get(logout)
 router.route('/user/update/:id').get(updateUser)  //api/v1/user/update/11
-router.route('/user/delete/:id').get(deleteUser)  //api/v1/user/delete/11
+router.route('/user/delete/:id').get(isAuthenticatedUser, deleteUser)  //api/v1/user/delete/11
 
 module.exports = router
